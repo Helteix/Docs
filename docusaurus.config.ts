92e19e01a3f43a8806efc04c89024f43acd88e54
Lupeni Tuikalepa <38043285+LupeniTuikalepa@users.noreply.github.com>
2025-06-17 19:23:31 +0200

    chef

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -15,15 +15,15 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://helteix.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/Docs/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'Helteix', // Usually your GitHub org/user name.
-  projectName: 'Helteix Unity Packages', // Usually your repo name.
+  projectName: 'Docs', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
